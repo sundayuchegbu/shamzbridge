2f257560710c381b0248ef2481c0f2d46792be6a
sundayuchegbu <sundayuchegbu10@gmail.com>
2024-10-19 08:03:11 +0100

    fixed margin with respect to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="font-sans tracking-wide bg-gray-500 py-10 px-10">
+    <footer className="font-sans tracking-wide bg-gray-500 py-2 md:-mt-72  px-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         <div>
           <h4 className="text-[#25aae1] font-semibold text-lg mb-6">
